cad57b40e368d6de9824e0f942157926dedfc47b
Vladimir Pekshev <enrichment.off@gmail.com>
2026-04-08 15:42:17 +0300

    add models

---
+++ internal/models/models.go
@@ -0,0 +1,45 @@
+﻿package models
+
+type InformationResponse struct {
+	Data struct {
+		SourceIdentifier       string   `json:"SourceIdentifier"`
+		ServerSupportedVersion []string `json:"ServerSupportedVersion"`
+	} `json:"Data"`
+}
+
+type SearchRequest struct {
+	Query string `json:"Query"`
+}
+
+type SearchResponse struct {
+	Data []PackageMatch `json:"Data"`
+}
+
+type PackageMatch struct {
+	Package struct {
+		Id        string `json:"Id"`
+		Name      string `json:"Name"`
+		Version   string `json:"Version"`
+		Publisher string `json:"Publisher"`
+	} `json:"Package"`
+}
+
+type ManifestResponse struct {
+	Data Manifest `json:"Manifest"`
+}
+
+type Manifest struct {
+	Id         string      `json:"Id"`
+	Name       string      `json:"Name"`
+	Version    string      `json:"Version"`
+	Publisher  string      `json:"Publisher"`
+	Installers []Installer `json:"Installers"`
+}
+
+type Installer struct {
+	InstallerUrl       string `json:"InstallerUrl"`
+	InstallerSha256    string `json:"InstallerSha256"`
+	InstallerType      string `json:"InstallerType"`
+	Silent             string `json:"Silent"`
+	SilentWithProgress string `json:"SilentWuthProgress"`
+}
